b4f4bb3ecdcb0b9a3751f4e82fe89b43b8f5476c
a4-adityaa <adityakushwaha9162@gmail.com>
2026-02-26 01:42:36 +0530

    Added chapter wise c++ program

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,25 @@
+// implement a selection sort;
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[6] = {5,4,3,2,1,0};
+    int n = 6;
+
+    for(int i=0; i<n-1; i++){
+        int minIndex = i;
+
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        swap(arr[i],arr[minIndex]);
+    }
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
