9c9c521dfffb1aa0b8f4ab27338dda7cfe2583f3
a4-adityaa <adityakushwaha9162@gmail.com>
2026-02-28 23:55:49 +0530

    Added Chapter wise c++ programs

---
+++ Chap-8/Linked list/PushBack.cpp
@@ -0,0 +1,55 @@
+// implement push_back using scrach;
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data= val;
+        next = NULL;
+    }
+};
+
+class list{
+    Node* head;
+    Node* tail;
+
+    public:
+    list(){
+        head=tail=NULL;
+    }
+    void push_back(int val){
+        Node* newNode = new Node(val);
+    
+       if(head == NULL){
+         head = tail = newNode;
+        } else{
+          tail->next = newNode;
+          tail = newNode;
+        }
+    }
+    void PrintLL(){
+        Node* temp = head;
+        while(temp != NULL){
+            cout<<temp->data<<" -> ";
+            temp = temp->next;
+        }
+        cout<<"NULL" << endl;
+    }
+};
+
+int main(){
+    list ll;
+    
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    
+    ll.PrintLL();
+    
+    return 0;
+}    
