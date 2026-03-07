8b485a0eb5f10a9e21d39681c5bac6c4b6885f74
a4-adityaa <adityakushwaha9162@gmail.com>
2026-03-07 18:34:21 +0530

    Added chapter wise c++ program

---
+++ Recursion/Syntax.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void printNum(int n){
+    if(n==1){
+        cout<<1<<endl;
+        return;
+    }
+    cout<<n<<" ";
+    printNum(n-1);
+}
+
+int main(){
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    printNum(n);
+    return 0;
+}
